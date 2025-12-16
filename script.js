28fda4344b796600070e66e15f4f2110edb7eb8c
Muhammad Ayaz <abubakar1house@gmail.com>
2025-12-16 20:10:02 +0500

    Uploading

---
+++ script.js
@@ -0,0 +1,60 @@
+var wind = document.getElementById("wind");
+var humidity = document.getElementById("humidity");
+var temperature = document.getElementById("temp");
+var cityName = document.getElementById("city");
+var input = document.getElementById("search_city");
+var searchBtn = document.getElementById("search_btn");
+var weatherIcon = document.querySelector(".weather_icon");
+var errorMessage = document.querySelector(".error_message");
+var weatherSection = document.querySelector(".weather_updates_section");
+
+// API Setup
+var apiKey = "3ac1790e21519f06101d6337766404c3";
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+// Main Function
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (response.status == 404 || response.status == 400) {
+        errorMessage.style.display = "block";
+        weatherSection.style.display = "none";
+    } else {
+        const data = await response.json();
+
+        temperature.innerHTML = Math.round(data.main.temp) + "ºc";
+        cityName.innerHTML = data.name;
+        humidity.innerHTML = data.main.humidity + "%";
+        wind.innerHTML = data.wind.speed + " Km/h";
+
+        const condition = data.weather[0].main;
+        if (condition === "Clouds") weatherIcon.src = "assets/clouds.png";
+        else if (condition === "Rain") weatherIcon.src = "assets/rain.png";
+        else if (condition === "Drizzle") weatherIcon.src = "assets/drizzle.png";
+        else if (condition === "Mist") weatherIcon.src = "assets/mist.png";
+        else if (condition === "Clear") weatherIcon.src = "assets/clear.png";
+        else if (condition === "Snow") weatherIcon.src = "assets/snow.png";
+        else weatherIcon.src = "assets/clear.png";
+
+        weatherSection.style.display = "block";
+        errorMessage.style.display = "none";
+    }
+}
+checkWeather("Islamabad")
+
+
+searchBtn.addEventListener("click", () => {
+    const city = input.value.trim();
+    if (city !== "") {
+        checkWeather(city);
+    }
+});
+
+input.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        const city = input.value.trim();
+        if (city !== "") {
+            checkWeather(city);
+        }
+    }
+});
